e30f8531b2d0835a68864f798017c3103ff0ea78
Coder_Kanon <carloseduardomendes201@gmail.com>
2023-07-26 12:04:47 -0300

    estrutura html para js

---
+++ script/quiz3.js
@@ -0,0 +1,150 @@
+const quiz = [
+    {
+        pergunta: 'Qual dos seguintes métodos é usado para exibir uma mensagem de alerta na tela do usuário?',
+        respostas: {
+            a: 'alert("Mensagem")',
+            b: 'confirm("Mensagem")',
+            c: 'log("Mensagem")',
+            d: 'prompt("Mensagem")'
+        },
+        respostaCorreta: "a"
+    },
+    {
+        pergunta: `O que o seguinte código irá imprimir no console? <br>
+        let x = 5; <br>
+        x = x + "5"; <br>
+        console.log(x);`,
+        respostas: {
+            a: '10',
+            b: '"55"',
+            c: '5',
+            d: '25'
+        },
+        respostaCorreta: "b"
+    },
+    {
+        pergunta: 'Qual é a maneira correta de declarar uma variável em JavaScript?',
+        respostas: {
+            a: 'variable nome = "João";',
+            b: 'var nome = "João";',
+            c: 'const nome = "João";',
+            d: 'let nome = "João";'
+        },
+        respostaCorreta: "d"
+    },
+    {
+        pergunta: 'Qual dos seguintes loops é mais adequado para percorrer os elementos de um array em JavaScript?',
+        respostas: {
+            a: 'for (let element of array)',
+            b: 'for (let i = 0; i; array.length; i++)',
+            c: 'while (array.length > 0)',
+            d: 'for (let i in array)'
+        },
+        respostaCorreta: "a"
+    },
+    {
+        pergunta: 'O que o operador de igualdade estrita === verifica em JavaScript?',
+        respostas: {
+            a: 'Compara os valores de duas variáveis, ignorando seus tipos.',
+            b: 'Verifica se uma variável é maior ou igual a outra.',
+            c: 'Compara os valores e os tipos de duas variáveis.',
+            d: 'Verifica se uma variável é menor ou igual a outra.'
+        },
+        respostaCorreta: "c"
+    },
+    {
+        pergunta: `Qual é o resultado da expressão abaixo?
+        x = "20" + 5`,
+        respostas: {
+            a: '25',
+            b: '205',
+            c: '"205"',
+            d: '255'
+        },
+        respostaCorreta: "c"
+    },
+    {
+        pergunta: `Em HTML, como você define o cabeçalho de uma tabela que se estende por várias colunas? <br>
+        const numbers = [1, 2, 3, 4, 5]; <br>
+        const result = numbers.map(num => num * 2); <br>
+        console.log(result);`,
+        respostas: {
+            a: '[1, 2, 3, 4, 5]',
+            b: '[2, 4, 6, 8, 10]',
+            c: '[1, 4, 9, 16, 25]',
+            d: '[2, 3, 4, 5, 6]'
+        },
+        respostaCorreta: "b"
+    },
+    {
+        pergunta: 'Qual dos seguintes métodos é usado para remover o último elemento de um array em JavaScript?',
+        respostas: {
+            a: 'array.pop()',
+            b: 'array.removeLast()',
+            c: 'array.add(elemento)',
+            d: 'array.shift()'
+        },
+        respostaCorreta: "a"
+    },
+    {
+        pergunta: 'Qual é a função do método addEventListener() em JavaScript?',
+        respostas: {
+            a: 'Adiciona um novo elemento ao DOM (Document Object Model).',
+            b: 'Remove um evento específico de um elemento.',
+            c: 'Adiciona um evento a um elemento, para que um determinado código seja executado quando o evento ocorrer.',
+            d: 'Cria um novo evento personalizado para o documento.'
+        },
+        respostaCorreta: "c"
+    },
+    {
+        pergunta: 'Qual é a função do operador typeof em JavaScript?',
+        respostas: {
+            a: 'Verificar se uma variável é nula.',
+            b: 'Verificar o tipo de uma variável.',
+            c: 'Verificar se uma variável é indefinida.',
+            d: 'Verificar se uma variável é um objeto.'
+        },
+        respostaCorreta: "b"
+    },
+];
+
+// Função para gerar as questões no HTML
+function gerarQuestoes() {
+    const perguntas = document.getElementById("containerPerguntas");
+
+    quiz.forEach((questao, index) => {
+        const div = document.createElement("div");
+        div.innerHTML = `<h3>${index + 1}. ${questao.pergunta}</h3>`;
+
+        for (let opcao in questao.respostas) {
+            div.innerHTML += `
+                <label>
+                    <input type="radio" name="pergunta${index}" value="${opcao}">
+                    ${questao.respostas[opcao]}
+                </label><br>
+            `;
+        }
+
+        div.innerHTML += '<br>';
+        perguntas.appendChild(div);
+    });
+}
+
+gerarQuestoes();
+
+// Função para calcular a pontuação total
+function calcularPontuacao() {
+    let pontuacao = 0;
+
+    quiz.forEach((questao, index) => {
+        const opcoes = document.getElementsByName(`pergunta${index}`);
+
+        opcoes.forEach(opcao => {
+            if (opcao.checked && opcao.value === questao.respostaCorreta) {
+                pontuacao++;
+            }
+        });
+    });
+
+    alert(`Sua pontuação: ${pontuacao}/${quiz.length}`);
+}
