07f4f0069d9be9a01495032cf83797f209818bfb
asassaki <alinesassaki@hotmail.com>
2023-07-30 21:09:39 -0300

    add tabela de resultados

---
+++ script/resultados.js
@@ -0,0 +1,54 @@
+const resultados1 = [];
+const resultados2 = [];
+const resultados3 = [];
+
+function exibirResultadosNaTabela() {
+  const tabelaResultados = document.getElementById('tabelaResultados');
+  const tbody = tabelaResultados.querySelector('tbody');
+
+  if (!tabelaResultados || !tbody) {
+    console.error('Tabela de resultados não encontrada.');
+    return;
+  }
+
+  tbody.innerHTML = '';
+
+  resultados1.forEach((resultado) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${resultado.nome}</td>
+      <td>${resultado.tema}</td>
+      <td>${resultado.tempo}</td>
+      <td>${resultado.data}</td>
+      <td>${resultado.pontuacao}</td>
+    `;
+    tbody.appendChild(tr);
+  });
+
+  resultados2.forEach((resultado) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${resultado.nome}</td>
+      <td>${resultado.tema}</td>
+      <td>${resultado.tempo}</td>
+      <td>${resultado.data}</td>
+      <td>${resultado.pontuacao}</td>
+    `;
+    tbody.appendChild(tr);
+  });
+
+  resultados3.forEach((resultado) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${resultado.nome}</td>
+      <td>${resultado.tema}</td>
+      <td>${resultado.tempo}</td>
+      <td>${resultado.data}</td>
+      <td>${resultado.pontuacao}</td>
+    `;
+    tbody.appendChild(tr);
+  });
+}
+
+
+
